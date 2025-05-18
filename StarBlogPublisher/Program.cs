5e8a9828a3ab420fffec306519edf61430896c49
DealiAxy <dealiaxy@gmail.com>
2025-05-18 10:53:16 +0800

    build: 添加Projektanker.Icons.Avalonia.FontAwesome依赖
    在项目中添加了Projektanker.Icons.Avalonia.FontAwesome依赖，并在MainWindow.axaml中引入了相应的命名空间，以支持FontAwesome图标的使用。同时，在Program.cs中注册了FontAwesome图标提供者，确保图标功能正常工作。

--- StarBlogPublisher/Program.cs
+++ StarBlogPublisher/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using System;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 
 namespace StarBlogPublisher;
 
@@ -12,9 +14,13 @@ sealed class Program {
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp() {
+        IconProvider.Current
+            .Register<FontAwesomeIconProvider>();
+
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+    }
 }
